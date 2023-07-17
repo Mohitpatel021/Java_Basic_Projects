d6cd9d20bb7f2aeaa2aa573f611a7e3b4aa379c0
CodeWithMohit <107213038+Mohitpatel021@users.noreply.github.com>
2023-07-17 15:07:12 +0530

    Update Project.java

--- Project.java
+++ Project.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 class GetInformations {
 
   Scanner sc = new Scanner(System.in);
-  // write function for getting information for registration
+  // Class variables
   String name;
   String fatherName;
   int age;
@@ -15,71 +15,42 @@ class GetInformations {
   double twelthPercentage;
   double graduationPercentage;
 
-  public void information(String companyName) {
-    System.out.println("Enter your Name :-");
-    name = sc.nextLine();
-
-    System.out.println("Enter your Father Name :-");
-    fatherName = sc.nextLine();
-
-    System.out.println("Enter your Phone Number :-");
-    phoneNumber = sc.nextDouble();
-
-    System.out.println("Enter your Email :-");
-    email = sc.next();
-
-    System.out.println("Enter your Permanent Address :-");
-    address = sc.next();
-    sc.next();
-
-    System.out.println("Enter your Age :-");
-    age = sc.nextInt();
-
-    if (age <= 18 || age >= 25) {
-      System.out.println("You are not eligible for Further process ");
-    } else {
-      System.out.println("Enter your  tenthPercentage :-");
-      tenthPercentage = sc.nextDouble();
-
-      System.out.println("Enter your  twelthPercentage :-");
-      twelthPercentage = sc.nextDouble();
-
-      System.out.println("Enter your  graduationPercentage :-");
-      graduationPercentage = sc.nextDouble();
-
-      if (
-        (tenthPercentage > 75) &&
-        (twelthPercentage > 75) &&
-        (graduationPercentage > 75)
-      ) {
-        System.out.println(
-          "Thankyou We've Received your Application for pilot registration for the company " +
-          companyName
-        );
-        System.out.println(
-          "If you will met our eligibility criteria, YOu will get an email notification for further instructions"
-        );
-      } else {
-        System.out.println(
-          "Your Qualification doesn't match our requirements "
-        );
-      }
-    }
+  // Constructor to initialize class variables
+   GetInformations(
+    String name,
+    String fatherName,
+    int age,
+    double phoneNumber,
+    String email,
+    String graduation,
+    String address,
+    double tenthPercentage,
+    double twelthPercentage,
+    double graduationPercentage
+  ) {
+    this.name = name;
+    this.fatherName = fatherName;
+    this.age = age;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+    this.graduation = graduation;
+    this.address = address;
+    this.tenthPercentage = tenthPercentage;
+    this.twelthPercentage = twelthPercentage;
+    this.graduationPercentage = graduationPercentage;
   }
 }
 
-public class Project {
+public class PilotRegistration{
 
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-
-    GetInformations obj1 = new GetInformations();
 
     System.out.println("=======================================");
     System.out.println("        PILOT REGISTRATION SYSTEM      ");
     System.out.println("=======================================");
 
-    System.out.println("Here is some Airlines Company Names:");
+    System.out.println("Here are some Airlines Company Names:");
 
     String[] airlineCompanies = {
       "IndiGo Airlines",
@@ -98,24 +69,93 @@ public class Project {
       "Air Pegasus",
       "Air Costa",
     };
-    //this will display all company names
+
+    // Display all company names
     for (String company : airlineCompanies) {
       System.out.println("-" + company);
     }
+    //prints company names
     System.out.println(
-      "Please Select one of the following Companies which you would like to Register yourself:-"
+      "Please select one of the following Companies which you would like to register yourself:"
     );
-    //here user will enter one company name
+    //choose company name
     String nameOfcompany = sc.nextLine();
-    //this will check if the company is in  the list or not
+    //checkk wheather company name present in the array or not
     for (int i = 0; i < airlineCompanies.length; i++) {
       if (airlineCompanies[i].equalsIgnoreCase(nameOfcompany)) {
-        //here user need to enter information for further registration process
         System.out.println(
-          "Please Provide Relevent Information for registration purpose :-"
+          "Please provide relevant information for registration purpose:"
         );
-        //here i want to call function for taking input from the user
-        obj1.information(nameOfcompany);
+        double tenthPercentage;
+        double twelthPercentage;
+        double graduationPercentage;
+        // Get input from the user for information
+        System.out.println("Enter your Name:");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your Father Name :-");
+        String fatherName = sc.nextLine();
+
+        System.out.println("Enter your Phone Number :-");
+        double phoneNumber = sc.nextDouble();
+
+        System.out.println("Enter your Email :-");
+        String email = sc.next();
+
+        System.out.println("Enter your Permanent Address :-");
+        String address = sc.next();
+        sc.next();
+        System.out.println("enter your graduation :-");
+        String graduation = sc.next();
+
+        System.out.println("Enter your Age :-");
+        int age = sc.nextInt();
+
+        if (age <= 18 || age >= 25) {
+          System.out.println("You are not eligible for Further process ");
+          break;
+        } else {
+          System.out.println("Enter your  tenthPercentage :-");
+          tenthPercentage = sc.nextDouble();
+
+          System.out.println("Enter your  twelthPercentage :-");
+          twelthPercentage = sc.nextDouble();
+
+          System.out.println("Enter your  graduationPercentage :-");
+          graduationPercentage = sc.nextDouble();
+
+          if (
+            (tenthPercentage > 75) &&
+            (twelthPercentage > 75) &&
+            (graduationPercentage > 75)
+          ) {
+            System.out.println(
+              "Thankyou We've Received your Application for pilot registration for the company " +
+              nameOfcompany
+            );
+            System.out.println(
+              "If you will met our eligibility criteria, YOu will get an email notification for further instructions"
+            );
+          } else {
+            System.out.println(
+              "Your Qualification doesn't match our requirements "
+            );
+          }
+        }
+
+        //Calling constructor for memory allocation
+        GetInformations obj1 = new GetInformations(
+          name,
+          fatherName,
+          age,
+          phoneNumber,
+          email,
+          graduation,
+          address,
+          tenthPercentage,
+          twelthPercentage,
+          graduationPercentage
+        );
         break;
       }
     }
